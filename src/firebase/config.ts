ccd5ef48eb128f21f3aacfa4c39c4fdf7b62a4a4
yash jain <yash.jain5194@gmail.com>
2026-01-27 14:15:33 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "ecommpilot-22-55961890-7ab48",
+  "appId": "1:365997967477:web:2fcaca95a9103bcb47d03c",
+  "apiKey": "AIzaSyB1-UgLaBIKpNe_wzkoHhhaCycZ6ybcx2w",
+  "authDomain": "ecommpilot-22-55961890-7ab48.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "365997967477"
+};
